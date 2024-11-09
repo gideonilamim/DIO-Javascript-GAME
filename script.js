842121cef315721ea6fe88bcc38738f362fc4b48
Gideoni LM <gid.lamim@gmail.com>
2024-11-08 21:34:53 -0300

    add an enemy panel

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 //addEventListener('load' --> when the page loads
 
 let playerSelector = document.getElementsByClassName('playerSelector');
-//console.log(playerSelector.id);
+let selectPlayerPanel = document.getElementById('selectPlayerPanel');
+let selectEnemyPanel = document.getElementById('selectEnemyPanel');
+
 
 const players  = [
   {
@@ -28,6 +30,39 @@ const players  = [
  
 ];
 
+const enemies  = [
+  {
+    name:'Dead Stump',
+    src: './images/7.jpg',
+    power : 40,
+    special : 80,
+    life: 1000
+  }, 
+  {
+    name:'monk',
+    src: './images/ninja.jpg',
+    power : 40,
+    special : 60,
+    life: 1000
+  },
+  {
+    name:'wizard',
+    src: './images/ninja.jpg',
+    power : 20,
+    special : 120,
+    life: 1000
+  },
+  {
+    name:'Warrior',
+    src: './images/ninja.jpg',
+    power : 50,
+    special : 60,
+    life: 1000
+  }
+ 
+ 
+];
+
 class Player {
   constructor(player) {
     this.player = player;
@@ -46,14 +81,39 @@ class Player {
   
 }
 
-function playerSelect (player) {
-  new Player(player);
+class Enemy {
+
+  constructor(enemy) {
+    this.enemy = enemy;
+    console.log("created a " + enemy.name);
+    this.src = enemy.src;
+    this.power = enemy.power;
+    this.special = enemy.special;
+    this.life = enemy.life;
+    console.log(this.power);
+  }
+
+  // attack mode
+  attack(context) {
+    
+  }
   
-  for (let i = 0; i < 4; i++){
-    playerSelector[i].classList.add("hide");
-}
 }
 
+function selectEnemy(){
+  new Enemy(enemies[0]);
+  console.log(selectEnemyPanel.getElementsByClassName('playerSelector'));
+  selectEnemyPanel.getElementsByClassName('enemy').src = './images/warrior.jpg';
+};
+
+//this is a function to select the player
+function playerSelect (player) {
+  new Player(player);
+  selectPlayerPanel.classList.add("hidden");
+  selectEnemy();
+}
+
+//to create a clickable image
 for (let i = 0; i < 4; i++){
     playerSelector[i].addEventListener("click", () =>{ playerSelect(players[i])});
 }
